340b65a31a5abc66410abc2a721acbc0c085856d
CJ <cjfritz9@gmail.com>
2023-03-09 12:37:45 -0700

    server warmup method testing

--- src/api/EmailAPI.ts
+++ src/api/EmailAPI.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 const BASE_URL = 'https://mail-server-379822.uc.r.appspot.com';
 
+export const serverWarmup = async () => {
+  axios.get(`${BASE_URL}/_ah/warmup`)
+}
+
 const sendMail = async (data: object) => {
-  axios.get(`${BASE_URL}/`)
   const response = await axios.post(`${BASE_URL}/api/send-mail`, data);
 
   if (response.data.success) {
--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Stack } from '@chakra-ui/react';
 import Hero from '../Hero';
 import ContactMe from '../ContactMe';
 import MailForm from '../MailForm';
+import { serverWarmup } from '../../api/EmailAPI';
 
 const Contact: React.FC = () => {
+  useEffect(() => {
+    serverWarmup();
+  }, []);
   return (
     <Stack>
       <Hero heading='CONTACT' />
