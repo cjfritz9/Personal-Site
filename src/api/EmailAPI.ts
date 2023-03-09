d1de7022048d3b81c07e52def37b4903b0773c11
CJ <cjfritz9@gmail.com>
2023-03-09 12:55:01 -0700

    server warmup method testing

--- src/api/EmailAPI.ts
+++ src/api/EmailAPI.ts
@@ -1,10 +1,6 @@
 import axios from 'axios';
 const BASE_URL = 'https://mail-server-379822.uc.r.appspot.com';
 
-export const serverWarmup = async () => {
-  axios.get(`${BASE_URL}/_ah/warmup`)
-}
-
 const sendMail = async (data: object) => {
   const response = await axios.post(`${BASE_URL}/api/send-mail`, data);
 
