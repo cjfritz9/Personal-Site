e2046840d9e54abdc88643bd27bcfe04a879ac81
CJ <cjfritz9@gmail.com>
2023-05-02 16:02:18 -0600

    updated email api endpoint to reflect changes made to the server

--- src/api/EmailAPI.ts
+++ src/api/EmailAPI.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 const BASE_URL = 'https://mail-server-379822.uc.r.appspot.com';
 
 const sendMail = async (data: object) => {
-  const response = await axios.post(`${BASE_URL}/api/send-mail`, data);
+  const response = await axios.post(`${BASE_URL}/personal/send`, data);
 
   if (response.data.success) {
     return { success: 'Your Message Was Sent'}
